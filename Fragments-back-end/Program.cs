bc654e8372c13e3967978766fc336a1adce168b7
Dammy99 <86931896+Dammy99@users.noreply.github.com>
2022-04-19 19:38:50 +0300

    Closes #2 - Connect Sonar Cloud to Fragments-back-end repository (#19)

---
+++ Fragments-back-end/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fragments_back_end
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
