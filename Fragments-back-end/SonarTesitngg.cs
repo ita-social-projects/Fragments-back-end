54fba8ffec3324b5e97538cf81002a11b73ee547
Dammy99 <deslyly.prod@gmail.com>
2022-04-19 20:24:04 +0300

    Initial Folders

---
+++ Fragments-back-end/SonarTesitngg.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        System.Console.WriteLine("Hello World!");
+        System.Console.WriteLine("Hello World!");
+        System.Console.WriteLine("Hello World!");
+        }
+    }
+}
