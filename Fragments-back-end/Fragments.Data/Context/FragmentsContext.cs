cbff4585a387c4436d461f1b2cd8edc2932cbeb4
Vladyslav Leskiv <vlad2199944@gmail.com>
2022-04-30 23:31:09 +0300

    Added FragmentsContext

---
+++ Fragments-back-end/Fragments.Data/Context/FragmentsContext.cs
@@ -0,0 +1,23 @@
+﻿using Fragments.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Fragments.Data.Context
+{
+    public class FragmentsContext : DbContext
+    {
+        public DbSet<User>? users { get; set; }
+
+        public FragmentsContext() 
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            string connectionString = builder.Build().GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
