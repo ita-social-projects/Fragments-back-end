108ee6f88528a6af316e9dbf04b0721acc2e61e1
Mark13371488 <81811476+Mark13371488@users.noreply.github.com>
2022-07-04 01:29:26 +0300

    test

---
+++ Fragments-back-end/Fragments.Test/Base/AutoEntityData.cs
@@ -0,0 +1,19 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fragments.Test.Base
+{
+    public class AutoEntityDataAttribute: AutoDataAttribute
+    {
+        public AutoEntityDataAttribute()
+            : base(() => new Fixture().Customize(new AutoMoqCustomization()))
+        {
+        }
+    }
+}
---
+++ Fragments-back-end/Fragments.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Fragments.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
