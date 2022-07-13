5414336fe984ef71aaabad7d689b19ac41d75386
Vladyslav Leskiv <vlad2199944@gmail.com>
2022-07-13 16:12:03 +0300

    Added Fluent validation for auth

---
+++ Fragments-back-end/Fragments.Domain/Validations/AuthenticateValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Fragments.Domain.Dto;
+using Fragments.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fragments.Domain.Validations
+{
+    public class AuthenticateValidator : AbstractValidator<AuthenticateRequestDTO>
+    {
+        public AuthenticateValidator(IUserService service)
+        {
+            RuleFor(x => x.Email).Must(email => service.IsEmailAlreadyExistsAsync(email).Result).WithMessage("User not found!");
+        }
+    }
+}
