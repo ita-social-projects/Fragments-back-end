840446993127910a0967b7de97e78700ff04c5ba
Vladyslav Leskiv <vlad2199944@gmail.com>
2022-07-28 22:42:45 +0300

    Create Notifications mapper

---
+++ Fragments-back-end/Fragments.Domain/Profiles/NotificationsProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Fragments.Data.Entities;
+using Fragments.Domain.Dto;
+
+namespace Fragments.Domain.Profiles
+{
+    public class NotificationsProfile : Profile
+    {
+        public NotificationsProfile()
+        {
+            CreateMap<Notifications, NotificationsDTO>().ReverseMap().ForMember(x => x.User, opt => opt.Ignore());
+
+            CreateMap<Notifications, NotificationsDTO>();
+
+        }
+    }
+}
