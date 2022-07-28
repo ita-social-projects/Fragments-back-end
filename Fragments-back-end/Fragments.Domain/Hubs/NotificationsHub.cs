be4df06f73760d8ec80731d112130a46d1c93ae2
Vladyslav Leskiv <vlad2199944@gmail.com>
2022-07-28 22:42:21 +0300

    Create NotificationsHub.cs

---
+++ Fragments-back-end/Fragments.Domain/Hubs/NotificationsHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Fragments.Domain.Hubs
+{
+    [Authorize]
+    public class NotificationsHub : Hub
+    {
+        public static string ConnectionId { get; private set; } = string.Empty;
+
+        public async Task EnterGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        }
+        public async Task ExitGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+        }
+        public async Task SendMessageToGroup(string groopName, string message)
+        {
+            await Clients.Group(groopName).SendAsync("RecieveMessage", message);
+        }
+        public override Task OnConnectedAsync()
+        {
+            ConnectionId = Context.ConnectionId;
+            return base.OnConnectedAsync();
+        }
+    }
+}
