8473df1e5f835205e882a8e65d33738f071e8552
Vladyslav Leskiv <vlad2199944@gmail.com>
2022-07-28 22:43:14 +0300

    Update INotificationService.cs

--- Fragments-back-end/Fragments.Domain/Services/Interfaces/INotificationService.cs
+++ Fragments-back-end/Fragments.Domain/Services/Interfaces/INotificationService.cs
@@ -1,8 +1,15 @@
 ﻿using Fragments.Data.Entities;
+using Fragments.Domain.Dto;
 
 namespace Fragments.Domain.Services.Interfaces
 {
     public interface INotificationService
     {
+        Task<NotificationsDTO> AddNotificationAsync(NotificationsDTO notification);
+
+        Task<bool> DeleteNotificationAsync(int notificationId);
+
+        Task<IReadOnlyList<NotificationsDTO>> GetNotificationsAsync();
+
     }
 }
