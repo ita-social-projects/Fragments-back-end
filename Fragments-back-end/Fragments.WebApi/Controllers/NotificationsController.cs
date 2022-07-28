358b6598ddff011389d996712478c3d11a386e0b
Vladyslav Leskiv <vlad2199944@gmail.com>
2022-07-28 22:43:19 +0300

    Update NotificationsController.cs

--- Fragments-back-end/Fragments.WebApi/Controllers/NotificationsController.cs
+++ Fragments-back-end/Fragments.WebApi/Controllers/NotificationsController.cs
@@ -1,6 +1,37 @@
-﻿namespace Fragments.WebApi.Controllers
+﻿using Fragments.Domain.Dto;
+using Fragments.Domain.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fragments.WebApi.Controllers
 {
-    public class NotificationsController
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationsController : ControllerBase
     {
+        private readonly INotificationService _notificationsService;
+
+        public NotificationsController(INotificationService notificationService)
+        {
+            _notificationsService = notificationService;
+        }
+
+        [HttpPost("create-notification")]
+        public async Task<IActionResult> CreateAsync(NotificationsDTO notification)
+        {
+            return Ok(await _notificationsService.AddNotificationAsync(notification));
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var deleted = await _notificationsService.DeleteNotificationAsync(id);
+            return deleted ? Ok() : Forbid();
+        }
+        [HttpGet("notifications")]
+        public async Task<IActionResult> GetNotificationsAsync()
+        {
+            return Ok(await _notificationsService.GetNotificationsAsync());
+        }
     }
 }
