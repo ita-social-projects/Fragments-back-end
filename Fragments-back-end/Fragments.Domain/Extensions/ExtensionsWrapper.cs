1b0d295f28b9b5480467475c7538655c59fdcf6e
Vladyslav Leskiv <vlad2199944@gmail.com>
2022-08-17 09:26:52 +0300

    Added wrapper class for extensions methods

---
+++ Fragments-back-end/Fragments.Domain/Extensions/ExtensionsWrapper.cs
@@ -0,0 +1,19 @@
+﻿using Fragments.Data.Entities;
+using Fragments.Domain.Helpers;
+using Microsoft.Extensions.Configuration;
+
+namespace Fragments.Domain.Extensions
+{
+    public class ExtensionsWrapper : IExtensionsWrapper
+    {
+        private readonly IConfiguration _configuration;
+        public ExtensionsWrapper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GetJwtToken(User user)
+        {
+            return _configuration.GenerateJwtToken(user);
+        }
+    }
+}
---
+++ Fragments-back-end/Fragments.Domain/Extensions/IExtensionsWrapper.cs
@@ -0,0 +1,9 @@
+﻿using Fragments.Data.Entities;
+
+namespace Fragments.Domain.Extensions
+{
+    public interface IExtensionsWrapper
+    {
+        string GetJwtToken(User user);
+    }
+}
